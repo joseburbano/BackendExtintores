656d091133889c4f52c7afb82cd0326981bc53eb
Jose Burbano <84249978+joseburbano@users.noreply.github.com>
2021-11-01 22:43:03 -0500

    arreglado

--- src/routes/control.routes.js
+++ src/routes/control.routes.js
@@ -1,12 +1,17 @@
 const { Router } = require("express");
-
+const { CacheMiddleware } = require("../middleware");
+const { CACHE_TIME } = require("../helpers");
 module.exports = function ({ ControlController }) {
   const router = Router();
 
   //CONTROL EXTINTOREES ES HOME INICIO
 
   //datos de inicio
-  router.get("/start-data/:fecha", ControlController.controlPanelData);
+  router.get(
+    "/start-data/:fecha",
+    [CacheMiddleware(CACHE_TIME.ONE_HOUR)],
+    ControlController.controlPanelData,
+  );
 
   return router;
 };
